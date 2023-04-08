ab32d620a4eafc0280f63a43af070ef82b1691af
arjun18027 <54967986+arjun18027@users.noreply.github.com>
2023-04-08 13:58:17 +0700

    Update book.py

--- book.py
+++ book.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 import os
@@ -6,6 +7,7 @@ import auto_subtitle
 from moviepy.editor import AudioFileClip, VideoFileClip, CompositeVideoClip, concatenate_videoclips
 import subprocess
 import shutil
+from pydub import AudioSegment
 
 # set folder paths
 audio_folder = 'audio'
@@ -50,7 +52,7 @@ for audio_file in audio_files:
 
     # buat subtitle dan simpan ke dalam folder subtitled dengan nama file yang sama dengan file audio
     subtitle_path = os.path.join(subtitled_folder, f"{os.path.splitext(audio_file)[0]}.srt")
-    os.system(f"auto_subtitle {os.path.join(output_folder, os.path.splitext(audio_file)[0] + '.mp4')} -o {subtitled_folder} --model small.en --srt_only true")
+    os.system(f"auto_subtitle {os.path.join(output_folder, os.path.splitext(audio_file)[0] + '.mp4')} -o {subtitled_folder} --model large")
 
      # tambahkan subtitle ke video
     subtitle_path = f"{subtitled_folder}/{os.path.splitext(audio_file)[0]}.srt"
@@ -58,11 +60,39 @@ for audio_file in audio_files:
     input_video_file = os.path.join(output_folder, f"{os.path.splitext(audio_file)[0]}.mp4")
     output_video_file = os.path.join(output_folder, f"{os.path.splitext(audio_file)[0]}_subtitled.mp4")
 
-    # gunakan perintah os.system untuk menjalankan perintah ffmpeg
-    cmd = f'ffmpeg -i "{input_video_file}" -i "output/audio.mp3" -filter_complex "[0:v]subtitles={subtitle_path}:force_style=\'Alignment=0,BorderStyle=3,Outline=1,Shadow=0,Fontsize=12,MarginL=20,MarginV=120,BorderColor=&H100000000\',scale=1080:1920[v];[1:a][0:a]amerge=inputs=2[a]" -map "[v]" -map "[a]" -c:v h264_amf -crf 1 -preset veryfast -c:a aac -shortest "{output_video_file}"'
-    os.system(cmd)
-    # Pindahkan file sementara ke file asal
-    shutil.move(output_video_file, input_video_file)
+# Membuka backsound dan mendapatkan durasinya
+backsound = AudioSegment.from_file("output/audio.mp3", format="mp3")
+backsound_duration = backsound.duration_seconds
 
-    # hapus file subtitle yang sudah digunakan
-    os.remove(subtitle_path)
+# loop untuk setiap file di folder subtitled
+for filename in os.listdir(subtitled_folder):
+    # cek apakah file adalah file video
+    if filename.endswith(VIDEO_EXTENSIONS):
+        # buka file video
+        video_path = os.path.join(subtitled_folder, filename)
+        video_clip = VideoFileClip(video_path)
+
+        # Mendapatkan durasi video
+        video_duration = video_clip.duration
+
+        # Memotong backsound jika durasinya lebih panjang dari video
+        if backsound_duration > video_duration:
+            backsound = backsound[:video_duration*1000]
+
+        # Mengulangi backsound sampai durasinya sama dengan video
+        while backsound_duration < video_duration:
+            backsound += backsound
+            backsound_duration = backsound.duration_seconds
+
+        # Mengedit level suara backsound
+        backsound = backsound - 20
+
+        # Menambahkan backsound ke video
+        video_with_backsound = video_clip.set_audio(backsound)
+
+        # Simpan video dengan backsound
+        result_path = os.path.join(subtitled_folder, filename)
+        video_with_backsound.write_videofile(result_path, codec="h264_amf")
+
+        # hapus file lama
+        os.remove(video_path)
